493535ece20fc359aa3b99706a8b2088b51de403
matthew-bo <matthewpieguy@gmail.com>
2025-02-22 17:27:58 -0500

    update security

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from '@/components/AuthProvider';
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Card Picker MVP",
@@ -16,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={inter.className}>
+    <html lang="en">
       <body>
         <AuthProvider>
           {children}
