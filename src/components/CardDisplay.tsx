e4f4fb7a8b25535b786dc9133c06328137789dc7
matthew-bo <matthewpieguy@gmail.com>
2025-02-23 23:58:34 -0500

    changes ui

--- src/components/CardDisplay.tsx
+++ src/components/CardDisplay.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { useAuth } from '@/components/AuthProvider';
+// import { useAuth } from '@/components/AuthProvider';
 
 // Add CreditCardDetails interface
 interface CreditCardDetails {
