a226a7de11e0be2df252207e8ec347069b603668
matthew-bo <matthewpieguy@gmail.com>
2025-03-03 23:18:34 -0500

    oo

--- src/app/recommender/page.tsx
+++ src/app/recommender/page.tsx
@@ -5,6 +5,7 @@ import { useAuth } from '@/components/AuthProvider';
 
 export default function RecommenderPage() {
   // Use underscore prefix to indicate an intentionally unused variable
+ // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { user: _user } = useAuth();
   
   // Simple loading state to prevent any issues during hydration
